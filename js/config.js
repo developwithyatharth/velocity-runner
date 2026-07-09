34da27780f986ad89e2858483ad7968f3af609c6
developwithyatharth <sharma.01yatharth@gmail.com>
2026-07-09 19:40:13 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,16 @@
+/* config.js
+   Game constants and basic settings
+*/
+
+const lanes = [-3, 0, 3];
+
+const TILE_DEPTH = 8;
+const ROAD_TILE_COUNT = 34;
+const ROAD_LOOP_DISTANCE = TILE_DEPTH * ROAD_TILE_COUNT;
+
+const SHOOT_COOLDOWN_MAX = 38;
+
+const START_SPEED = 0.34;
+const GRAVITY = -0.035;
+
+const BOSS_DISTANCE_GAP = 1000;
